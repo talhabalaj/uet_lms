9435cfad3f52b76bd705d8ef4a18dc0c1a73e74e
Talha Balaj <talhabalaj@hotmail.com>
2020-12-14 14:31:24 +0500

    Update

--- lib/ui/views/splash_view/splash_view.dart
+++ lib/ui/views/splash_view/splash_view.dart
@@ -1,23 +1,48 @@
-
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:uet_lms/ui/views/splash_view/spash_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
 class SplashView extends StatelessWidget {
   static final id = "/";
-  
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.reactive(
       builder: (context, model, _) {
         return Scaffold(
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: !model.internet
+                ? MainAxisAlignment.end
+                : MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(model.internet ? "LOADING YOUR STUFF" : "NO INTERNET, BRO. ", style: TextStyle(fontSize: 20)),
-              SizedBox(height: 30),
-              Lottie.asset("assets/lottie/${model.internet ? "loading" : "no-internet"}.json", fit: BoxFit.contain),
+              Container(),
+
+                SizedBox(height: 30),
+              if (!model.internet) ...[
+                Text(
+                  "sad, i can't connect to internet",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 30),
+              ],
+              Flexible(
+                child: Lottie.asset(
+                  "assets/lottie/${model.internet ? "loading" : "no-internet"}.json",
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+              if (model.internet)
+                Shimmer.fromColors(
+                  baseColor: Colors.grey[700],
+                  highlightColor: Colors.grey[400],
+                  child: Text(
+                    "dOwnLoaDing nuCLEAr COdes",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
             ],
           ),
         );
